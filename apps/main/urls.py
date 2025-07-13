f4ecc7c7178802986a2c5e51a0eb9096f314a0f3
VolodymyrMamaevsky <vova.moder@gmail.com>
2025-07-13 16:37:40 +0300

    feat: add rest api for CV model with DRF

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from main.views import CVDetailView, CVListView
+from main.views import CVDetailView, CVListView, CVViewSet, export_cv_pdf
+
+router = DefaultRouter()
+router.register(r"api/cv", CVViewSet, basename="cv")
 
 urlpatterns = [
     path("", CVListView.as_view(), name="cv_list"),
     path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
+    path("cv/<int:pk>/pdf/", export_cv_pdf, name="cv_pdf"),
 ]
+
+urlpatterns += router.urls
 
 app_name = "main"
