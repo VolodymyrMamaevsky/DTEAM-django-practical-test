4420db5059c2eabe763c0b273e3b4e6bb5fdd3d7
VolodymyrMamaevsky <vova.moder@gmail.com>
2025-07-13 14:00:15 +0300

    feat: add CV detail view route and implement list view template

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import CVListView
+from main.views import CVDetailView, CVListView
 
 urlpatterns = [
     path("", CVListView.as_view(), name="cv_list"),
+    path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
 ]
 
 app_name = "main"
